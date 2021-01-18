684b9aa69fd3cf1ff7ff07835cd4d3addef876c7
wangsiyuan <2650306917@qq.com>
2021-01-18 13:05:08 +0800

    Initial commit

---
+++ src/main/java/com/wang/covid_19_sys/Covid19SysApplication.java
@@ -0,0 +1,13 @@
+package com.wang.covid_19_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19SysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Covid19SysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wang/covid_19_sys/Covid19SysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.covid_19_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19SysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
